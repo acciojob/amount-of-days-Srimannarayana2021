832587eec9f64ddad7f81eacd0b6d2bcff4c8d06
Srimannarayana <87487895+Srimannarayana2021@users.noreply.github.com>
2025-04-06 13:55:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+  // A leap year is:
+  // - divisible by 4
+  // - NOT divisible by 100 unless also divisible by 400
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return 366;
+  } else {
+    return 365;
+  }
+}
